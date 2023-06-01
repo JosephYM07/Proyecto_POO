4f2d2c48bd699a6ff082cceac3d0352899c66915
Joseph Yepez <josepym07@gmail.com>
2023-06-01 18:46:48 -0500

    Anexo creacion de objetos

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     static int opcion = 0;
+
     public static void main(String[] args) {
         while (opcion != 7) {
             opcion = mostrarMenuPrincipal();
@@ -53,6 +54,7 @@ public class Main {
         }
         return opcion;
     }
+
     public static void submenuAccion() {
         // objetos de la clase Peliculas
         Peliculas pelicula1 = new Peliculas("Chad Stahelski", "Español", 2019, 131, "Nominado a Premio Razzie a la Redención (Keanu Reeves)", "Keanu Reeves", "John Wick (Keanu Reeves) regresa a la acción, solo que esta vez con una recompensa\n de 14 millones de dólares sobre su cabeza y con un ejército de mercenarios intentando darle caza.\n Tras asesinar a uno de los miembros del gremio de asesinos al que pertenecía, Wick es expulsado de la organización, \npasando a convertirse en el centro de atención de multitud de asesinos a sueldo que esperan detrás de\n cada esquina para tratar de deshacerse de él.");
@@ -100,10 +102,12 @@ public class Main {
 
     public static void submenuAventura() {
         int opcion;
+        Peliculas peliculas3 = new Peliculas("Jake Kasdan", "Español", 2017, 119, "Nominado a Premio Razzie a la Redención (Dwayne Johnson)", "Dwayne Johnson", "Cuatro adolescentes son absorbidos por un videojuego, en el que se convierten en avatares de personajes arquetípicos. \nAllí vivirán múltiples aventuras, al tiempo que buscan cómo salir de allí para volver a su mundo.");
+        Peliculas peliculas4 = new Peliculas("Gavin Hood", "Inglés", 2013, 114, "19 Critics Choice Awards Mejor intérprete joven(Asa Butterfield", "Asa Butterfield", "es una película estadounidense del 2013 de ciencia ficción basada en la novela homónima de Orson Scott Card. \nEstá dirigida y escrita por Gavin Hood y los actores principales son Asa Butterfield ―en el papel de Andrew Ender Wiggin,\n un chico con talento inusual enviado a una avanzada escuela militar en el espacio para prepararse en la lucha contra las invasiones alienígenas―,\n Harrison Ford, Ben Kingsley, Hailee Steinfeld, Viola Davis, Abigail Breslin, Moisés Arias y Nonso Anozie.");
         do {
             System.out.println("\n\tAcción");
-            System.out.println("1. Detalles de la película 1");
-            System.out.println("2. Detalles de la película 2");
+            System.out.println("1. Detalles de la película Jumanji: En la selva");
+            System.out.println("2. Detalles de la película El juego de Ender");
             System.out.println("3. Volver al menú principal");
             System.out.print("Ingresa tu opción: ");
             opcion = scanner.nextInt();
@@ -116,10 +120,20 @@ public class Main {
             }
             switch (opcion) {
                 case 1:
-                    System.out.println("DETALLES DE LA PELÍCULA 1");
+                    do {
+                        System.out.println("DETALLES DE LA PELÍCULA Jumanji: En la selva");
+                        mostrarDetallesPelicula(peliculas3);
+                        System.out.println("Desea continuar en esta seccion 1.-Si 2.-No:");
+                        opcion = scanner.nextInt();
+                    } while (opcion == 1);
                     break;
                 case 2:
-                    System.out.println("DETALLES DE LA PELÍCULA 2");
+                    do {
+                        System.out.println("DETALLES DE LA PELÍCULA El juego de Ender");
+                        mostrarDetallesPelicula(peliculas4);
+                        System.out.println("Desea continuar en esta seccion 1.-Si 2.-No:");
+                        opcion = scanner.nextInt();
+                    } while (opcion == 1);
                     break;
                 case 3:
                     System.out.println("Volviendo al menú principal...");
@@ -247,6 +261,7 @@ public class Main {
             }
         } while (opcion != 3);
     }
+
     //Métodos para mostrar los detalles de las películas
     public static void mostrarDetallesPelicula(Peliculas pelicula) {
         System.out.println("\nDetalles de la película:");
